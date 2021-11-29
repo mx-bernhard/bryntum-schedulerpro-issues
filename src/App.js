3be63638d554b687851ec9ebb856cdc245499899
Bernhard Jahn <bernhard.jahn@mobilexag.de>
2021-11-29 17:10:32 +0100

    before-update attempt

--- src/App.js
+++ src/App.js
@@ -15,7 +15,25 @@ import {
 import { schedulerConfig } from './AppConfig';
 
 const App = () => {
-    return <BryntumSchedulerPro {...schedulerConfig} />;
+    const ref = React.useRef();
+    const refCallback = React.useCallback((scheduler) => {
+        ref.current = scheduler;
+        if (ref.current?.instance != null) {
+            // ref.current.instance.eventStore.addListener('catchAll', (event) => {
+            //     console.log('eventStore event:', event);
+            //     // return false;
+            // });
+            ref.current.instance.eventStore.addListener('beforeUpdate', event => {
+                console.log('beforeUpdate: ', event);
+                return false;
+            })
+            // ref.current.instance.project.addListener('catchAll', (event) => {
+            //     console.log('project event:', event);
+
+            // })
+        }
+    }, []);
+    return <BryntumSchedulerPro ref={refCallback} {...schedulerConfig} />;
 };
 
 export default App;
--- src/AppConfig.js
+++ src/AppConfig.js
@@ -2,24 +2,28 @@
  * Application configuration
  */
 import {
+    SchedulerProjectModel,
     ProjectModel,
     StringHelper
 } from '@bryntum/schedulerpro/schedulerpro.umd';
 
-const project = new ProjectModel({
-    autoLoad: true,
-});
-
+const project = new ProjectModel({ });
+project.resourceStore.data = [
+    {
+        "id": 1,
+        "name": "Angelo"
+    }
+];
 const schedulerConfig = {
     project,
     startDate: new Date(2020, 10, 29),
     endDate: new Date(2021, 0, 10),
     rowHeight: 50,
     barMargin: 2,
-
     viewPreset: 'weekAndDay',
     resourceImagePath: 'users/',
-
+    // taskEditFeature: false,
+    //eventEditFeature: false,
     columns: [
         {
             text: 'Resource',
